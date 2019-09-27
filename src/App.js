572a46568ac4aec6c5f31d44a5a440cfc2b6fbcd
Neo <ksssugar@gmail.com>
2019-09-27 10:43:22 +0800

    added collection-item component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
-import './App.css';
+import ShopPage from './pages/shop-page/shop-page.component';
 import HomePage from './pages/homepage/homepage.component';
 
-const HatsPage = (props) => (
-  <div>
-    <h1>Hats Page</h1>
-  </div>
-);
+import './App.css';
+
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   )
