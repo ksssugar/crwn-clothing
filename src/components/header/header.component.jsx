8e926155f5d8c3efe78ee3b3940a361e304f8782
Neo <ksssugar@gmail.com>
2019-10-07 23:47:22 +0800

    updating our app to redirect to home if user is signed in

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -32,7 +32,7 @@ const Header = ({ currentUser }) => (
 );
 
 const mapStateToProps = state => ({
-  currentUser: state.user.currentUSer
+  currentUser: state.user.currentUser
 })
 
 
--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -1,10 +1,12 @@
-const INITIALIZE_STATE = {
+import { UserActionTypes } from './user.types';
+
+const INITIAL_STATE = {
     currentUser: null
 };
 
-const userReducer = (state = INITIALIZE_STATE, action) => {
+const userReducer = (state = INITIAL_STATE, action) => {
     switch (action.type) {
-        case 'SET_CURRENT_USER':
+        case UserActionTypes.SET_CURRENT_USER:
             return {
                 ...state,
                 currentUser: action.payload
