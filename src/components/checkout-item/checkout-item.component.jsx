729fc6de36d200759f89a2f9acc4ee0f8b127b9b
Neo <ksssugar@gmail.com>
2019-10-10 01:26:08 +0800

    added addItem and and removeItem functionality to checkout item component

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,17 +1,33 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { clearItemFromCart, removeItem, addItem } from '../../redux/cart/cart.actions';
 
 import './checkout-item.styles.scss';
 
-const CheckoutItem = ({ item: {imageUrl, name, price, quantity} }) => (
-    <div className='checkout-item'>
-        <div className='image-container'>
-            <img src={imageUrl} alt="item"/>
-        </div>
-        <span className='name'>{name}</span>
-        <span className='quantity'>{quantity}</span>
-        <span className='price'>{price}</span>
-        <span className='remove'>&#10005;</span>
-    </div>
-);
+const CheckoutItem = ({ item, clearItem, addItem, removeItem }) => {
+    const { imageUrl, name, price, quantity } = item;
 
-export default CheckoutItem;
+    return (
+        <div className='checkout-item'>
+            <div className='image-container'>
+                <img src={imageUrl} alt='item'/>
+            </div>
+            <span className='name'>{name}</span>
+            <span className='quantity'>
+                <div className='arrow' onClick={() => removeItem(item)}>&#10094;</div>
+                <span className='value'>{quantity}</span>
+                <div className='arrow' onClick={() => addItem(item)}>&#10095;</div>
+            </span>
+            <span className='price'>{price}</span>
+            <div className='remove-button' onClick={() => clearItem(item)}>&#10005;</div>
+        </div>
+    )
+};
+
+const mapDispatchToProps = dispatch => ({
+    clearItem: item => dispatch(clearItemFromCart(item)),
+    removeItem: item => dispatch(removeItem(item)),
+    addItem: item => dispatch(addItem(item))
+});
+
+export default connect(null, mapDispatchToProps)(CheckoutItem);
