17b6cb64914d50fcbb6afe70bc8064cfad2c59a1
Neo <ksssugar@gmail.com>
2019-10-22 14:58:10 +0800

    moved shop.data.js and directory state to their respective reducers

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import store from './redux/store';
+import { store, persistor } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
@@ -11,7 +12,9 @@ import App from './App';
 ReactDOM.render(
     <Provider store={store}>
         <BrowserRouter>
-            <App /> 
+            <PersistGate persistor={persistor}>
+                <App /> 
+            </PersistGate>
         </BrowserRouter>
     </Provider>,
     document.getElementById('root')
--- src/pages/shop-page/shop-page.component.jsx
+++ src/pages/shop-page/shop-page.component.jsx
@@ -1,26 +1,24 @@
 import React from 'react';
-import SHOP_DATA from './shop.data';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
 import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+import { selectCollections } from '../../redux/shop/shop.selector';
 
 import './shop-page.styles.scss';
 
 
-class ShopPage extends React.Component {
-    state = { collections: SHOP_DATA }
+const ShopPage = ({ collections }) => (
+    <div className="shop-page">
+        {
+            collections.map(({id, ...otherCollectionProps}) =>
+                    <CollectionPreview key={id} {...otherCollectionProps} />)
+        }
+    </div>
+);
 
-    render() {
-        const {collections} = this.state;
-        
-        return (
-            <div className="shop-page">
-                {
-                    collections.map(({id, ...otherCollectionProps}) =>
-                         <CollectionPreview key={id} {...otherCollectionProps} />)
-                }
-            </div>
-        )
-    }
-}
+const mapStateToProps = createStructuredSelector({
+    collections: selectCollections
+});
 
-export default ShopPage;
+export default connect(mapStateToProps)(ShopPage);
