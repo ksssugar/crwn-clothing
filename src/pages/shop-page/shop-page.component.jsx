6ab48d3a79300a824558855a4aaf02e932a23ccc
Neo <ksssugar@gmail.com>
2019-10-23 18:29:09 +0800

    added stripe button, updated the ShopPage with CollectionPage component, change the favicon and react app title

--- src/pages/shop-page/shop-page.component.jsx
+++ src/pages/shop-page/shop-page.component.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+import CollectionPage from '../collection-page/collection-page.component';
 
 import './shop-page.styles.scss';
 
 
-const ShopPage = () => (
+const ShopPage = ({ match }) => (
     <div className="shop-page">
-        <CollectionsOverview />
+        <Route exact path={`${match.path}`} component={CollectionsOverview} />
+        <Route exact path={`${match.path}/:collectionId`} component={CollectionPage} />
     </div>
 );
 
